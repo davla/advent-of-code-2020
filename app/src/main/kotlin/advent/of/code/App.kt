b7ddef7b35d440c4a17e689b63d95ff3fff96940
Davide Laezza <truzzialrogo@gmx.com>
2020-12-04 23:17:53 +0100

    Adding day3/puzzle2 solution

--- app/src/main/kotlin/advent/of/code/App.kt
+++ app/src/main/kotlin/advent/of/code/App.kt
@@ -5,6 +5,7 @@ fun main(args: Array<String>) {
         Pair("day2", "puzzle1") -> Day2.puzzle1(args.slice(2..(args.size - 1)))
         Pair("day2", "puzzle2") -> Day2.puzzle2(args.slice(2..(args.size - 1)))
         Pair("day3", "puzzle1") -> Day3.puzzle1(args.slice(2..(args.size - 1)))
+        Pair("day3", "puzzle2") -> Day3.puzzle2(args.slice(2..(args.size - 1)))
         else -> null
     }
     println(result)
--- app/src/main/kotlin/advent/of/code/Day3.kt
+++ app/src/main/kotlin/advent/of/code/Day3.kt
@@ -17,6 +17,14 @@ object Day3 {
         }
     }
 
+    private val coordsProgressions = listOf<Pair<(Int) -> Int, (Int) -> IntProgression>>(
+        Pair({ it + 1 }, { 0 until it }),
+        Pair({ it + 3 }, { 0 until it }),
+        Pair({ it + 5 }, { 0 until it }),
+        Pair({ it + 7 }, { 0 until it }),
+        Pair({ it + 1 }, { 0 until it step 2 })
+    )
+
     private fun parseLine(line: String) =
         line.map(Tile::fromChar)
 
@@ -25,14 +33,25 @@ object Day3 {
             .map(::parseLine)
             .toList()
 
-    private fun navigate(map: Collection<Collection<Tile>>): Int =
+    private fun navigate(map: Collection<Collection<Tile>>,
+                         nextX: (Int) -> Int): Int =
         map.fold(Pair(0, 0)) { (trees, x), line ->
             Pair(
                 if (line.elementAt(x) == Tile.TREE) trees + 1 else trees,
-                (x + 3) % line.size
+                nextX(x) % line.size
             )
         }.first
 
     fun puzzle1(args: Iterable<String>) =
-        navigate(parseInput(args.first())
+        navigate(parseInput(args.first()), { it + 3 })
+
+    fun puzzle2(args: Iterable<String>) =
+        parseInput(args.first())
+            .let { lines ->
+                coordsProgressions.map { (nextX, yIndices) ->
+                    navigate(lines.toList().slice(yIndices(lines.size)), nextX)
+                }
+            }.map(Int::toLong)
+            .reduce(Long::times)
+
 }
