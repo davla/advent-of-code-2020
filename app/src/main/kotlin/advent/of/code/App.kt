7c872eee4c07267f8f102a5faa6d30b288351eaf
Davide Laezza <truzzialrogo@gmx.com>
2020-12-04 23:37:40 +0100

    Adding day4/puzzle2 solution

--- app/src/main/kotlin/advent/of/code/App.kt
+++ app/src/main/kotlin/advent/of/code/App.kt
@@ -7,6 +7,7 @@ fun main(args: Array<String>) {
         Pair("day3", "puzzle1") -> Day3.puzzle1(args.slice(2..(args.size - 1)))
         Pair("day3", "puzzle2") -> Day3.puzzle2(args.slice(2..(args.size - 1)))
         Pair("day4", "puzzle1") -> Day4.puzzle1(args.slice(2..(args.size - 1)))
+        Pair("day4", "puzzle2") -> Day4.puzzle2(args.slice(2..(args.size - 1)))
         else -> null
     }
     println(result)
--- app/src/main/kotlin/advent/of/code/Day4.kt
+++ app/src/main/kotlin/advent/of/code/Day4.kt
@@ -15,14 +15,14 @@ object Day4 {
     private fun <T> Iterable<T>.partitionWhile(predicate: (T) -> Boolean): Pair<Iterable<T>, Iterable<T>> =
         Pair(takeWhile(predicate), dropWhile(predicate))
 
-    private data class Passport(val birthYear: Int?,
-                                val issueYear: Int?,
-                                val expirationYear: Int?,
-                                val height: Pair<Int, String>?,
-                                val hairColor: String?,
-                                val eyeColor: String?,
-                                val passportID: String?,
-                                val countryID: Int?) {
+    private class Passport(val birthYear: Int?,
+                           val issueYear: Int?,
+                           val expirationYear: Int?,
+                           val height: Pair<Int, String>?,
+                           val hairColor: String?,
+                           val eyeColor: String?,
+                           val passportID: String?,
+                           val countryID: Int?) {
         val isValid = birthYear != null
             && issueYear != null
             && expirationYear != null
@@ -62,6 +62,27 @@ object Day4 {
         }
     }
 
+    private class StrictPassport(private val passport: Passport) {
+        val isValid = passport.isValid
+            && passport.birthYear.toString().length == 4
+            && passport.birthYear in 1920..2002
+            && passport.issueYear.toString().length == 4
+            && passport.issueYear in 2010..2020
+            && passport.expirationYear.toString().length == 4
+            && passport.expirationYear in 2020..2030
+            && when (passport.height!!.second) {
+                "cm" -> passport.height.first in 150..193
+                "in" -> passport.height.first in 59..76
+                else -> false
+            }
+            && passport.hairColor!!.length == 7
+            && passport.hairColor.startsWith("#")
+            && passport.hairColor.drop(1).toIntOrNull(radix = 16) != null
+            && passport.eyeColor in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+            && passport.passportID!!.length == 9
+            && passport.passportID.toIntOrNull() != null
+    }
+
     private fun parseInput(path: String) =
         File(path).readLines().asSequence()
             .split(String::isBlank)
@@ -71,5 +92,10 @@ object Day4 {
     fun puzzle1(args: Iterable<String>) =
         parseInput(args.first())
             .count(Passport::isValid)
+
+    fun puzzle2(args: Iterable<String>) =
+        parseInput(args.first())
+            .map(::StrictPassport)
+            .count(StrictPassport::isValid)
 
 }
