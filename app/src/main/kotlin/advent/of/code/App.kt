01fb31b5cca5d75ab0bd853cb047b2ad71fb4ae3
Davide Laezza <truzzialrogo@gmx.com>
2020-12-26 22:14:25 +0100

    Adding day14/puzzle2 solutions

--- app/src/main/kotlin/advent/of/code/App.kt
+++ app/src/main/kotlin/advent/of/code/App.kt
@@ -59,6 +59,7 @@ fun main(args: Array<String>) {
         Pair("day13", "puzzle1") -> Day13.puzzle1(lines, puzzleArgs)
         Pair("day13", "puzzle2") -> Day13.puzzle2(lines, puzzleArgs)
         Pair("day14", "puzzle1") -> Day14.puzzle1(lines, puzzleArgs)
+        Pair("day14", "puzzle2") -> Day14.puzzle2(lines, puzzleArgs)
         else -> null
     }
     println(result)
--- app/src/main/kotlin/advent/of/code/Day14.kt
+++ app/src/main/kotlin/advent/of/code/Day14.kt
@@ -1,5 +1,7 @@
 package advent.of.code
 
+import kotlin.math.pow
+
 private sealed class Instruction {
     companion object {
         fun parse(line: String): Instruction {
@@ -22,22 +24,7 @@ private sealed class Instruction {
     }
 }
 
-private class Mask(bits: String) : Instruction() {
-    val ones = bits.map {
-        when (it) {
-            'X' -> '0'
-            else -> it
-        }
-    }.joinToString(separator = "").toLong(radix = 2)
-    val zeroes = bits.map {
-        when (it) {
-            'X' -> '1'
-            else -> it
-        }
-    }.joinToString(separator = "").toLong(radix = 2)
-
-    fun apply(n: Long) = ones or (zeroes and n)
-}
+private data class Mask(val bits: String) : Instruction()
 
 private data class Mem(val address: Long, val value: Long) : Instruction() {
     companion object {
@@ -46,15 +33,17 @@ private data class Mem(val address: Long, val value: Long) : Instruction() {
 }
 
 object Day14 {
-    private class CPU {
-        private lateinit var currentBitMask: Mask
-        private val memory = mutableMapOf<Long, Long>()
+    private abstract class CPU {
+        protected lateinit var currentBitMask: Mask
+        protected val memory = mutableMapOf<Long, Long>()
+
+        abstract protected fun executeMem(mem: Mem)
 
         fun execute(instructions: Sequence<Instruction>) {
             instructions.forEach {
                 when (it) {
                     is Mask -> currentBitMask = it
-                    is Mem -> memory.put(it.address, currentBitMask.apply(it.value))
+                    is Mem -> executeMem(it)
                 }
             }
         }
@@ -62,8 +51,78 @@ object Day14 {
         fun memorySum() = memory.values.sum()
     }
 
+    private class CPU1 : CPU() {
+        private fun applyMask(value: Long): Long {
+            val orMask = currentBitMask.bits.map {
+                when (it) {
+                    'X' -> '0'
+                    else -> it
+                }
+            }.joinToString(separator = "").toLong(radix = 2)
+
+            val andMask = currentBitMask.bits.map {
+                when (it) {
+                    'X' -> '1'
+                    else -> it
+                }
+            }.joinToString(separator = "").toLong(radix = 2)
+
+            return orMask or (andMask and value)
+        }
+
+        override protected fun executeMem(mem: Mem) {
+            memory.put(mem.address, applyMask(mem.value))
+        }
+    }
+
+    private class CPU2 : CPU() {
+        private fun applyMask(address: Long): Sequence<Long> {
+            val addressBits = address.toString(radix = 2)
+                .padStart(currentBitMask.bits.length, '0')
+            val withX = addressBits.zip(currentBitMask.bits)
+                { addressBit, maskBit ->
+                    when (maskBit) {
+                        '0' -> addressBit
+                        else -> maskBit
+                    }
+                }
+            return expandX(withX.joinToString(separator = ""))
+        }
+
+        override protected fun executeMem(mem: Mem) {
+            applyMask(mem.address).forEach { address ->
+                memory.put(address, mem.value)
+            }
+        }
+
+        companion object {
+            private fun expandX(mask: String): Sequence<Long> {
+                val xIndices = mask.withIndex()
+                    .filter { it.value == 'X' }
+                    .map { it.index }
+                val xCount = xIndices.size
+                val xBits = (0..2.0.pow(xCount).toLong())
+                    .asSequence()
+                    .map { it.toString(radix = 2).padStart(xCount, '0') }
+                return xBits.map {
+                        xIndices.zip(it.asIterable()).fold(mask)
+                            { acc, (maskIndex, xBit) ->
+                                acc.replaceRange(maskIndex, maskIndex + 1,
+                                    xBit.toString())
+                            }
+                    }
+                    .map { it.toLong(radix = 2) }
+            }
+        }
+    }
+
     fun puzzle1(inputLines: Sequence<String>, args: Collection<String>) =
-        CPU()
+        CPU1()
+            .apply { execute(inputLines.map(Instruction::parse)) }
+            .memorySum()
+
+    fun puzzle2(inputLines: Sequence<String>, args: Collection<String>) =
+        CPU2()
             .apply { execute(inputLines.map(Instruction::parse)) }
             .memorySum()
 }
